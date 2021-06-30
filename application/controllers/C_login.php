6337e796ab0dd51355c7d033fce3f995e665df11
friski <friskisatya5@gmail.com>
2021-06-30 17:03:12 +0700

    final

--- application/controllers/C_login.php
+++ application/controllers/C_login.php
@@ -119,8 +119,8 @@ class C_login extends CI_Controller {
 					'charset'   => 'utf-8',
 					'protocol'  => 'smtp',
 					'smtp_host' => 'smtp.gmail.com',
-					'smtp_user' => '',  // Email gmail
-					'smtp_pass'   => '',  // Password gmail
+					'smtp_user' => 'friskisatya5@gmail.com',  // Email gmail
+					'smtp_pass'   => '06121998nugrahafriski',  // Password gmail
 					'smtp_crypto' => 'ssl',
 					'smtp_port'   => 465,
 					'crlf'    => "\r\n",
--- application/views/artikel.php
+++ application/views/artikel.php
@@ -1,7 +1,23 @@
-<div class="container">
-    <h6 class="title float-right btn-sm btn-primary" onclick="window.location='<?=base_url('C_index')?>'"><i class="fas fa-arrow-left"></i>&nbsp Back</h6>
-    <h4 class="title"><i class="fas fa-book-open"></i>&nbsp artikel</h4>
-    <div class="card">
+<!-- banner application -->
+<div class="card-wrapper bg-white text-dark">
+<div class="row m-4">
+<div class="col-6"></div>
+<div class="col-6"> 
+            </div>
+        </div>
+    <div class="card-img-overlay mt-5 p-0" style="height : 35%">
+        <div class="row m-4">
+            <div class="col-8">
+                <h4 class="card-title">&nbsp Artikel</h4>
+            </div>
+            <div class="col-4">
+            <h6 class="float-right btn-sm btn-info ml-1 bg-info" onclick="window.location='<?=base_url('C_index')?>'"><i class="fas fa-arrow-left"></i>&nbsp Back</h6>  
+            </div>
+        </div>
+    </div>
+</div>
+    <div class="card-wrapper">
+    <h4 class="card-title text-left"><?= $rs_artikel[0]->judul_artikel?></h4>
     <?php if($rs_artikel[0]->img_artikel!=""||$rs_artikel[0]->img_artikel!=null){?>
         <img id="previewImg" src="<?= base_url('uploads/').$rs_artikel[0]->img_artikel?>" alt="pic">
     <?php
@@ -10,13 +26,15 @@
         <img class="card-img-top" src="<?= base_url()?>/assets/img/batuk.jpg" alt="Card image cap">
     <?php } ?>
         <div class="card-body">
-            <h4 class="card-title text-left"><?= $rs_artikel[0]->judul_artikel?></h4>
+            <br>
             <p class="card-text text-justify"><?= $rs_artikel[0]->isi_artikel?></p>
-    </div><hr>
-    <h4>Baca Artikel Lainnya</h4>
+            <hr>
+    </div>
+    </div>
+    <h4 class="title h5">Baca Artikel Lainnya</h4>
     <div class="row text-center flex-nowrap" style="overflow: auto;">
     <?php foreach($rs_artikel_all as $artikel){?>
-        <div class="btn btn-sm col-auto m-1 p-0 pb-2">
+        <div class="btn btn-sm col-auto m-1 p-0 pb-2 bg-light">
             <div class="card-wrapper" style="width: 20rem;">
             <?php if($artikel->img_artikel!=""||$artikel->img_artikel!=null) { ?>
                 <img class="card-img-top" height="180" src="<?= base_url('uploads/').$artikel->img_artikel?>" alt="Card image cap">
